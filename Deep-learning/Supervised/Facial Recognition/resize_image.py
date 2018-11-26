75af7795586d92b574af6e68c15f437a00a29641
Sandeep Kumar <sandeepkumaramni@gmail.com>
2018-11-26 17:41:36 +0530

    Update resize_image.py

--- Deep-learning/Supervised/Facial Recognition/resize_image.py
+++ Deep-learning/Supervised/Facial Recognition/resize_image.py
@@ -1,14 +1,12 @@
 import glob
 import cv2
-#cv_img = []
-#img_dir = "rj/" # Enter Directory of all images 
-#data_path = os.path.join(img_dir,'*g')
-#files = glob.glob(data_path)
+
+folder_name = "example/*.jpg"
 i = 0 
-for img in glob.glob("kanna/*.jpg"):
+for img in glob.glob(folder_name):
     n= cv2.imread(img)
     m = cv2.resize(n,(640,480))
     name = str(i)
     cv2.imwrite("kannan/"+name+".jpg",m)
     i=i+1
-    #cv_img.append(n)`
+    
