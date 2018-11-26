9f3eef48f9536e7e3b86868d658930d116602f21
Sandeep Kumar <sandeepkumaramni@gmail.com>
2018-11-26 13:31:07 +0530

    Training and inference code added

---
+++ Deep-learning/Supervised/Sop_generator/Training.py
@@ -0,0 +1,55 @@
+from numpy import array
+from pickle import dump
+from keras.preprocessing.text import Tokenizer
+from keras.utils import to_categorical
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+from keras.layers import Embedding
+from keras.layers import Dropout,Bidirectional
+
+# load doc into memory
+def load_doc(filename):
+	# open the file as read only
+	file = open(filename, 'r')
+	# read all text
+	text = file.read()
+	# close the file
+	file.close()
+	return text
+
+# load
+in_filename = 'sop_converted.txt'
+doc = load_doc(in_filename)
+lines = doc.split('\n')
+
+# integer encode sequences of words
+tokenizer = Tokenizer()
+tokenizer.fit_on_texts(lines)
+sequences = tokenizer.texts_to_sequences(lines)
+# vocabulary size
+vocab_size = len(tokenizer.word_index) + 1
+
+# separate into input and output
+sequences = array(sequences)
+X, y = sequences[:,:-1], sequences[:,-1]
+y = to_categorical(y, num_classes=vocab_size)
+seq_length = X.shape[1]
+
+# define model
+model = Sequential()
+model.add(Embedding(vocab_size, 100, input_length=seq_length))
+model.add(LSTM(75, return_sequences=True))
+model.add(LSTM(75))
+model.add(Dense(75, activation='relu'))
+model.add(Dense(vocab_size, activation='softmax'))
+print(model.summary())
+
+# compile model
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+# fit model
+model.fit(X, y, batch_size=60, epochs=10)
+
+model.save('sop_text_generator.h5')
+# save the tokenizer
+dump(tokenizer, open('tokenizer.pkl', 'wb'))
---
+++ Deep-learning/Supervised/Sop_generator/clean.py
@@ -0,0 +1,60 @@
+import string
+
+# load doc into memory
+def load_doc(filename):
+	# open the file as read only
+	file = open(filename, 'r')
+	# read all text
+	text = file.read()
+	# close the file
+	file.close()
+	return text
+
+# turn a doc into clean tokens
+def clean_doc(doc):
+	# replace '--' with a space ' '
+	doc = doc.replace('--', ' ')
+	# split into tokens by white space
+	tokens = doc.split()
+	# remove punctuation from each token
+	table = str.maketrans('', '', string.punctuation)
+	tokens = [w.translate(table) for w in tokens]
+	# remove remaining tokens that are not alphabetic
+	tokens = [word for word in tokens if word.isalpha()]
+	# make lower case
+	tokens = [word.lower() for word in tokens]
+	return tokens
+
+# save tokens to file, one dialog per line
+def save_doc(lines, filename):
+	data = '\n'.join(lines)
+	file = open(filename, 'w')
+	file.write(data)
+	file.close()
+
+# load document
+in_filename = 'sop_sample.txt'
+doc = load_doc(in_filename)
+print(doc[:200])
+
+# clean document
+tokens = clean_doc(doc)
+print(tokens[:200])
+print('Total Tokens: %d' % len(tokens))
+print('Unique Tokens: %d' % len(set(tokens)))
+
+# organize into sequences of tokens
+length = 50 + 1
+sequences = list()
+for i in range(length, len(tokens)):
+	# select sequence of tokens
+	seq = tokens[i-length:i]
+	# convert into a line
+	line = ' '.join(seq)
+	# store
+	sequences.append(line)
+print('Total Sequences: %d' % len(sequences))
+
+# save sequences to file
+out_filename = 'sop_converted.txt'
+save_doc(sequences, out_filename)
